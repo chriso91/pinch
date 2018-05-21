8a63035af8e71a8634c182e345bde6165efc9bf3
chriso91 <christine.odea@t1cg.com>
2018-05-20 20:09:30 -0400

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, 'src');
@@ -11,8 +12,7 @@ module.exports = {
   ],
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js',
-    publicPath: "/pinch/"
+    filename: 'bundle.js'
   },
   module: {
     rules: [
